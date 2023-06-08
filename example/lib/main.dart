6e0315538e2e8eda27aab7736dd2c63bf3045218
punkzberryz <16286841+punkzberryz@users.noreply.github.com>
2023-06-08 15:42:06 +0800

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:papai_cloud_firestore_api/papai_cloud_firestore_api.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp(
+    fakeInstance: FakeFireBaseCloudFireStoreAPI(),
+    realInstance: FireBaseCloudFireStoreAPI(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp(
+      {super.key, required this.fakeInstance, required this.realInstance});
+
+  final CloudFireStore realInstance;
+  final CloudFireStore fakeInstance;
 
   @override
   Widget build(BuildContext context) {
@@ -15,37 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
-      ),
+      home: const Text('Hi'),
     );
   }
 }
